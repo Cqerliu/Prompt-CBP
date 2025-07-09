e3d7c4986a1bae0e1039d4f93505be967f6cbda7
Cqerliu <68686034+Cqerliu@users.noreply.github.com>
2025-07-09 19:51:10 +0800

    Add files via upload

---
+++ DataSet/MyDataset.py
@@ -0,0 +1,37 @@
+# This file is used to encapsulate data and convert DNA sequence data into computer recognizable numbers (tokens)
+
+import torch
+import torch.utils.data as Data
+
+# Convert sequences to tokens and pad the sequence length
+def input_token(train_sentences, train_labels, test_sentences, test_labels, tokenizer):
+    train_inputs = [tokenizer(sentence, return_tensors='pt')["input_ids"].squeeze() for sentence in train_sentences]
+    test_inputs = [tokenizer(sentence, return_tensors='pt')["input_ids"].squeeze() for sentence in test_sentences]
+    inputs = train_inputs + test_inputs
+    inputs = torch.nn.utils.rnn.pad_sequence(inputs, batch_first=True, padding_value=tokenizer.pad_token_id)
+    train_inputs = inputs[:len(train_inputs)]
+    test_inputs = inputs[len(train_inputs):]
+
+    return train_inputs, train_labels, test_inputs, test_labels
+
+
+def input_token_test(test_sentences, test_labels, tokenizer):
+    test_inputs = [tokenizer(sentence, return_tensors='pt')["input_ids"].squeeze() for sentence in test_sentences]
+    test_inputs = torch.nn.utils.rnn.pad_sequence(test_inputs, batch_first=True, padding_value=tokenizer.pad_token_id)
+
+    return test_inputs, test_labels
+
+
+class MyDataset(Data.Dataset):
+    def __init__(self, sentences, labels):
+        self.sentences = sentences
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.sentences)
+
+    def __getitem__(self, index):
+        inputs = self.sentences[index]
+        label = self.labels[index]
+        return inputs, label
+
